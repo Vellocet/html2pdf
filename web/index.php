3b1f743428d2e0e4765a05e7a0e6915df8260dca
Vellocet <a@a.com>
2014-10-07 15:33:47 -0700

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use mikehaertl\wkhtmlto\Pdf;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/', function() use($app) {
+    return $app['twig']->render('index.twig');
+});
+
+$app->get('/convert/{url}', function($url) {
+    $pdf = new Pdf($url);
+    $pdf->binary = __DIR__ . '/../vendor/bin/wkhtmltopdf-amd64';
+    $pdf->send("report.pdf");
+});
+
+$app['debug'] = true;
+// ... definitions
+//
+$app->run();
